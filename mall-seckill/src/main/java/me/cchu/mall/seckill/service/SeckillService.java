2750876206e39a90dc4e2a0eb6bfe750073d533d
mecchu <hischoolboy@gmail.com>
2021-04-12 00:35:44 +0800

    update

---
+++ mall-seckill/src/main/java/me/cchu/mall/seckill/service/SeckillService.java
@@ -0,0 +1,24 @@
+package me.cchu.mall.seckill.service;
+
+import me.cchu.mall.seckill.to.SeckillSkuRedisTo;
+
+import java.util.List;
+
+/**
+ * <p>Title: SeckillService</p>
+ * Description：
+ * date：2020/7/6 17:32
+ */
+public interface SeckillService {
+
+	void uploadSeckillSkuLatest3Day();
+
+	/**
+	 * 获取当前可以参与秒杀的商品信息
+	 */
+	List<SeckillSkuRedisTo> getCurrentSeckillSkus();
+
+	SeckillSkuRedisTo getSkuSeckillInfo(Long skuId);
+
+	String kill(String killId, String key, Integer num);
+}
